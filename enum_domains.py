b81c1636e4f64de9b0ba38f14502c9ff1db0d7b1
TimeShatter <timeshatter@qq.com>
2019-01-24 13:01:00 +0800

    添加了从文件中读取域名进行枚举的功能

--- enum_domains.py
+++ enum_domains.py
@@ -15,9 +15,16 @@ monkey.patch_all()
 
 class EnumerationSubDomain:
 
-    def __init__(self, domain, sub_dicts_file, dns_servers=None, coroutine_count=200, is_loop_query=True,
-            out_file=None):
+    def __init__(self, sub_dicts_file, domain=None, dns_servers=None, coroutine_count=200, is_loop_query=True,
+            out_file=None, domains_file=None):
+        if domain == None and domains_file == None:
+            raise RuntimeError('must set domain or domains_file! use -d or -f args!')
+
         self.domain = domain
+        self.domains = []
+        if domain:
+            self.domains.append(domain)
+
         self.sub_dicts = self.load_sub_dicts(sub_dicts_file)
         self.sub_domains = self.generate_sub_domains(domain, self.sub_dicts)
         if dns_servers == None:
@@ -30,6 +37,8 @@ class EnumerationSubDomain:
         self.coroutine_count = coroutine_count
         self.is_loop_query = is_loop_query
         self.out_file = out_file
+        self.domains_file = domains_file
+        self.load_sub_domains_from_file(self.domains_file)
         self.invalid_ip = ['0.0.0.1', '127.0.0.1']
 
         self.last_domains = [domain]
@@ -39,6 +48,25 @@ class EnumerationSubDomain:
         # current query domain count
         self.current_query_count = 0
 
+    def no_repeat_not_sort(self, lists):
+        return list(set(lists))
+
+    def no_repeat_sort(self, lists):
+        result = list(set(lists))
+        result.sort()
+        return result
+
+    def load_sub_domains_from_file(self, domains_file):
+        if domains_file:
+            with open(domains_file, 'r') as f:
+                domains = f.readlines()
+                for domain in domains:
+                    domain = domain.strip()
+                    self.domains.append(domain)
+                    self.sub_domains.extend(self.generate_sub_domains(domain, self.sub_dicts))
+            self.sub_domains = self.no_repeat_not_sort(self.sub_domains)
+            self.domains = self.no_repeat_not_sort(self.domains)
+    
     def load_sub_dicts(self, sub_dicts_file):
         sub_dicts = []
         with open(sub_dicts_file, 'r') as f:
@@ -61,8 +89,13 @@ class EnumerationSubDomain:
         for dns_server in dns_servers:
            resolver = dns.resolver.Resolver()
            resolver.nameservers = [dns_server]
+           resolver.timeout = 1
+           resolver.lifetime = 1
            start_time = time.time()
-           answer = resolver.query('baidu.com', 'A')
+           try:
+               answer = resolver.query('baidu.com', 'A')
+           except Exception as e:
+               pass
            end_time = time.time()
            total_time = end_time - start_time
            self.print_msg('dns_server: %s use %f to query ...' % (dns_server, total_time))
@@ -90,18 +123,21 @@ class EnumerationSubDomain:
             return True
 
     def generate_sub_domains(self, domain, sub_dicts):
-        sub_dicts = list(set(sub_dicts))
-        self.print_msg('load %d sub domain dicts ...' % len(sub_dicts))
-        sub_domains = []
-        if not self.is_domain(domain):
-            raise RuntimeError('%s not a domain' % domain)
-        sub_domains.append(domain)
-        for sub in sub_dicts:
-            sub_domain = sub + '.' + domain
-            if self.is_domain(sub_domain):
-                sub_domains.append(sub_domain)
-        self.print_msg('generate %s sub domains with %s ...' % (len(sub_domains), domain))
-        return sub_domains
+        if domain == None:
+            return []
+        else:
+            sub_dicts = self.no_repeat_not_sort(sub_dicts)
+            self.print_msg('load %d sub domain dicts ...' % len(sub_dicts))
+            sub_domains = []
+            if not self.is_domain(domain):
+                raise RuntimeError('%s not a domain' % domain)
+            sub_domains.append(domain)
+            for sub in sub_dicts:
+                sub_domain = sub + '.' + domain
+                if self.is_domain(sub_domain):
+                    sub_domains.append(sub_domain)
+            self.print_msg('generate %s sub domains with %s ...' % (len(sub_domains), domain))
+            return sub_domains
 
     def query(self, domain):
         # dns_server = self.dns_servers[random.randint(0,len(self.dns_servers) - 1)]
@@ -141,8 +177,7 @@ class EnumerationSubDomain:
         for domain in domains:
             sub = domain.split('.')[0]
             sub_list.append(sub + '\n')
-        sub_list = list(set(sub_list))
-        sub_list.sort()
+        sub_list = self.no_repeat_sort(sub_list)
         with open('my_sub_dicts.txt', 'w') as f:
             f.writelines(sub_list)
         new_num = len(sub_list)
@@ -196,12 +231,17 @@ class EnumerationSubDomain:
         if self.out_file:
             file_name = self.out_file
         else:
-            file_name = self.domain + time.strftime("_%Y%m%d%H%M%S", time.gmtime(time.time())) + '.txt'
+            if self.domain:
+                file_name = self.domain + time.strftime("_%Y%m%d%H%M%S", time.gmtime(time.time())) + '.txt'
+            else:
+                file_name = self.domains_file + time.strftime("_%Y%m%d%H%M%S", time.gmtime(time.time())) + '.txt'
         domain_names = self.domain_dict.keys()
         with open(file_name,'w') as f:
-            for domain in domain_names:
-                f.write(domain + ' , ' + ' , '.join(self.domain_dict[domain]) + '\n')
-        print 'all sub domain write to %s !' % file_name
+            for domain in self.domains:
+                for sub_domain in domain_names:
+                    if sub_domain.endswith(domain):
+                        f.write(sub_domain + ' , ' + ' , '.join(self.domain_dict[sub_domain]) + '\n')
+        self.print_msg('all sub domain write to %s !' % file_name)
 
     def enumerate(self):
         start_time = time.time()
@@ -212,13 +252,15 @@ class EnumerationSubDomain:
         end_time = time.time()
         total_time = int(end_time - start_time)
         self.print_msg('%s found %d sub domain ! The time used is %d seconds!' % (self.domain, len(self.domain_dict), total_time))
+        self.write_sub_domains_to_file()
         return self.domain_dict
 
 def parse_args():
     parser = argparse.ArgumentParser(description='A tool that can enumerate subdomains and support enumeration of subdomains above level 3')
-    parser.add_argument('-d','--domain', metavar='qq.com', dest='domain', type=str, required=True, help=u'domain to enumeration')
-    parser.add_argument('-f','--dict',metavar='subnames.txt',dest='dict', type=str, default='subdomains.txt', help=u'Subdomain dictionary')
+    parser.add_argument('-d','--domain', metavar='qq.com', dest='domain', type=str, help=u'domain to enumeration. if use -f, don\'t need to set this arg.')
+    parser.add_argument('-df','--dict-file',metavar='subnames.txt',dest='dict_file', type=str, default='subdomains.txt', help=u'Subdomain dictionary')
     parser.add_argument('-o','--out-file',metavar='domain.txt',dest='out_file', type=str, help=u'the file to write the result')
+    parser.add_argument('-f','--domains-file',metavar='domains.txt',dest='domains_file', type=str, help=u'Read the domain name to be enumerated from this file')
     parser.add_argument('-t','--thread',metavar='200',dest='coroutine_count', type=int, default=200, help=u'the count of thread')
     parser.add_argument('-n','--no-loop', dest='is_loop_query', action='store_false', default=True, help=u'Whether to enable circular query')
     parser.add_argument('--dns-server', metavar='8.8.8.8', dest='dns_servers', type=str, help=u'dns server')
@@ -227,25 +269,21 @@ def parse_args():
 
 def main():
    args = parse_args()
-   sub_dicts_file = args.dict
+   sub_dicts_file = args.dict_file
    domain = args.domain
    is_loop_query = args.is_loop_query
    coroutine_count = args.coroutine_count
    dns_servers = args.dns_servers
    out_file = args.out_file
-   enum_subdomain = EnumerationSubDomain(domain, sub_dicts_file, coroutine_count=coroutine_count, is_loop_query=is_loop_query,
-               dns_servers=dns_servers, out_file=out_file)
-   sub_domains = {}
+   domains_file = args.domains_file
+   enum_subdomain = EnumerationSubDomain(sub_dicts_file, domain, coroutine_count=coroutine_count, is_loop_query=is_loop_query,
+               dns_servers=dns_servers, out_file=out_file, domains_file=domains_file)
    try:
-       sub_domains = enum_subdomain.enumerate()
+       enum_subdomain.enumerate()
    except KeyboardInterrupt as e:
-       print 'user abort!'
-       file_name = domain + time.strftime("_%Y%m%d%H%M%S", time.gmtime(time.time())) + '.txt'
+       enum_subdomain.print_msg('user abort !')
        enum_subdomain.write_sub_domains_to_file()
-       return
+       enum_subdomain.improve_dicts(enum_subdomain.get_domains_list())
 
-   file_name = domain + time.strftime("_%Y%m%d%H%M%S", time.gmtime(time.time())) + '.txt'
-   enum_subdomain.write_sub_domains_to_file()
-   
 if __name__ == '__main__':
     main()
